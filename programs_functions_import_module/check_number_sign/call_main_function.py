f3e133902cb5b553fd439dbf1fca3c6a9740cad5
shrutiphaltankar <shrutibhedasgaonkar@gmail.com>
2024-10-11 21:19:10 +0100

    new program added

---
+++ programs_functions_import_module/check_number_sign/call_main_function.py
@@ -0,0 +1,7 @@
+from check_positive_negative_zero import *
+
+num = int(input("Enter any number (+ve, -ve, or zero) = "))
+
+result = check_number_sign(num)
+
+print("The given number", num, " is ", result)
---
+++ programs_functions_import_module/check_number_sign/check_positive_negative_zero.py
@@ -0,0 +1,8 @@
+def check_number_sign(n):
+    if n > 0:
+        return "Positive"
+    elif n < 0:
+        return "Negative"
+    else:
+        return "Zero"
+
